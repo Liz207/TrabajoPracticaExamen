1d33c8e674d1a0b50628dac1126f56330fb88fd7
ingri <ingri@Star>
2025-08-19 14:02:32 -0600

    commits 5

---
+++ src/application/exception/PrecioInvalidoException.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package application.exception;
+
+/**
+ *
+ * @author ingri
+ */
+public class PrecioInvalidoException extends Exception{
+     public PrecioInvalidoException(String message) { super(message); }
+}
+
---
+++ src/application/exception/StockNegativoException.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package application.exception;
+
+/**
+ *
+ * @author ingri
+ */
+public class StockNegativoException extends Exception{
+     public StockNegativoException(String message) { super(message); }
+}
+
