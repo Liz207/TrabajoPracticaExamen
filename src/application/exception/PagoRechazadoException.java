bee62e0f17f3cb63823a3ba537573f8991cd228f
ingri <ingri@Star>
2025-08-19 14:05:56 -0600

    commits 6

---
+++ src/application/exception/PagoRechazadoException.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package application.exception;
+
+/**
+ *
+ * @author ingri
+ */
+public class PagoRechazadoException extends Exception{
+    public PagoRechazadoException(String message) { super(message); }
+}
+
